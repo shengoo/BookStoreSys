58b2d850d76ed0d997e9f2f741c2246ac9696e61
shengoo <shengoo@hotmail.com>
2014-06-21 01:39:28 +0800

    added model
    added userrole model

---
+++ src/src/com/cuc/model/UserRole.java
@@ -0,0 +1,18 @@
+package com.cuc.model;
+
+public class UserRole {
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	private String type;
+}
